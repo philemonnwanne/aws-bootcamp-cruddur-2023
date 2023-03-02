5458c1493b9d217dd591a2037d3c3b902f927218
philemonnwanne <philemonnwanne@gmail.com>
2023-03-02 09:16:01 +0100

    switched environment to cruddur_test 👨🏾‍💻

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -96,7 +96,7 @@ def init_rollbar():
         # access token
         rollbar_access_token,
         # environment name
-        'cruddurtest',
+        'cruddur_test',
         # server root directory, makes tracebacks prettier
         root=os.path.dirname(os.path.realpath(__file__)),
         # flask already sets up logging
