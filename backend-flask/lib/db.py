8db83c0d2d61baa87f51a927eae268adea8e4f84
philemonnwanne <philemonnwanne@gmail.com>
2023-04-19 12:24:33 +0100

    implement create activity 👾

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,5 +1,7 @@
 from psycopg_pool import ConnectionPool
 import os # to load env vars
+import re,sys
+from flask import current_app as app
 
 class Db:
   def __init__(self):
@@ -9,29 +11,63 @@ class Db:
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
 
+  def print_params(self,params):
+    purple = '\033[95m'
+    no_color = '\033[0m'
+    print(f'{purple} SQL PARAMS:{no_color}')
+    for key, value in params.items():
+      print(key,":",value)
+       
+  def print_sql(self,title,sql):
+    cyan = '\033[96m'
+    no_color = '\033[0m'
+    print(f'{cyan} SQL STATEMENT-[{title}]-----{no_color}')
+    print(sql)
+
+  def template (self,*args):
+    pathing = list((app.root_path, 'db', 'sql') + args)
+    pathing[-1] = pathing[-1] + ".sql"
+
+    template_path = os.path.join(*pathing)
+
+    green = '\033[92m'
+    no_color = '\033[0m'
+    print("\n")
+    print(f'{green} LOAD SQL TEMPLATE: {template_path} {no_color}')
+    with open(template_path, 'r') as f:
+      template_content = f.read()
+    return template_content
+
   # we want to commit data such as an insert
-  def query_commit(self,sql):
+  # be sure to check for RETURNING in all uppercases 
+  def query_commit (self,sql,params={}):
+    self.print_sql('commit with returning', sql)
+    pattern = r"\bRETURNING\b"
+    is_returning_id = re.search(pattern, sql)
     try:
-      conn = self.pool.connection()
-      cur = conn.cursor()
-      cur.execute(sql)
-      cur.commit()
+      with self.pool.connection() as conn:
+        cur = conn.cursor()
+        cur.execute(sql,params)
+        if is_returning_id:
+          returning_id = cur.fetchone()[0]
+        conn.commit( )
+        if is_returning_id:
+          return returning_id
     except Exception as err:
       self.print_sql_err(err)
       # conn.rollback()
 
   # when we want to return a json object
-  def query_array_json(self,sql):
-    print("SQL STATEMENT-[array]-----")
-    print(sql + "\n")
+  def query_array_json(self,sql,params={}):
+    self.print_sql('array', sql)
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql)
+        cur.execute(wrapped_sql,params)
         # this will return a tuple
         # the first field being the data
         json = cur.fetchone()
-    return json[0]
+        return json[0]
   
   def query_wrap_array(self,template):
     sql = f"""
@@ -42,17 +78,20 @@ class Db:
     return sql
       
   # when we want to return an array of json objects
-  def query_object_json(self,sql):
-    print("SQL STATEMENT-[array]-----")
-    print(sql + "\n")
+  def query_object_json(self,sql,params={}):
+    self.print_sql('json',sql)
+    self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
-        cur.execute(wrapped_sql)
+        cur.execute(wrapped_sql,params)
         # this will return a tuple
         # the first field being the data
         json = cur.fetchone()
-        return json[0]
+        if json == None:
+          "{}" 
+        else:
+          return json[0] 
 
   def query_wrap_object(self,template):
     sql = f"""
@@ -64,7 +103,7 @@ class Db:
 
   def print_sql_err(self,err):
     # get details about the exception
-    err_type, err_obj, traceback = sys.exc_info()
+    err_type, traceback = sys.exc_info()
 
     # get the line number where the exception occurred
     line_num = traceback.tb_lineno
@@ -73,11 +112,11 @@ class Db:
     print("\npsycopg ERROR:", err, "on line number:", line_num)
     print("psycopg traceback:", traceback, "-- type:", err_type)
 
-    # psycopg2 extensions.Diagnostics object attribute
-    print("\nextensions.Diagnostics:", err.diag)
+    # # psycopg2 extensions.Diagnostics object attribute
+    # print("\nextensions.Diagnostics:", err.diag)
 
     # print the pgcode and pgerror exceptions
     print("pgerror:", err.pgerror)
-    print("pgcode:", err.pgcode, "\n")
-
+    print("pgcode:", err.pgcode, "\n") 
+ 
 db = Db()
