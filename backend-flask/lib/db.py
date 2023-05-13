7ad75574b9b6d2538f53ff3cf1a6771f35e47d70
philemonnwanne <philemonnwanne@gmail.com>
2023-05-13 09:34:24 +0100

    finished creating dynamodb utility scripts

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -18,11 +18,11 @@ class Db:
     for key, value in params.items():
       print(key,":",value)
        
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql,params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]-----{no_color}')
-    print(sql)
+    print(sql,params)
 
   def template (self,*args):
     pathing = list((app.root_path, 'db', 'sql') + args)
@@ -57,9 +57,18 @@ class Db:
       self.print_sql_err(err)
       # conn.rollback()
 
+ # when we want to return a a single value
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
+      
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
-    self.print_sql('array', sql)
+    self.print_sql('array',sql,params)
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
@@ -79,7 +88,7 @@ class Db:
       
   # when we want to return an array of json objects
   def query_object_json(self,sql,params={}):
-    self.print_sql('json',sql)
+    self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
     with self.pool.connection() as conn:
